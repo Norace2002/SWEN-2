fade9c171295a44921a8ce503bb1fda5ef724947
Raphael <pusam.raphy@gmail.com>
2024-03-15 17:42:29 +0100

    update: loading pages extended to route-specific menu points

--- src/main/java/at/fhtw/tourPlanner/view/RouteMenuController.java
+++ src/main/java/at/fhtw/tourPlanner/view/RouteMenuController.java
@@ -1,25 +1,63 @@
 package at.fhtw.tourPlanner.view;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RouteMenuController implements Initializable {
+
+    @FXML
+    Pane contentWindowPane;
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {}
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadGeneralWindow();
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void loadGeneralWindow(){
-        System.out.println("general window load");
+        System.out.println("load general window");
+
+        try{
+            contentWindowPane.getChildren().clear();
+
+            Pane newLoadedPane =  FXMLLoader.load(getClass().getResource("/at/fhtw/tourPlanner/generalWindow.fxml"));
+            contentWindowPane.getChildren().add(newLoadedPane);
+        } catch(Exception e){
+            System.out.println("Problem loading FXML into Pane");
+            e.printStackTrace();
+        }
     }
 
     public void loadRouteWindow(){
-        System.out.println("route window load");
+        System.out.println("load route window");
+
+        try{
+            contentWindowPane.getChildren().clear();
+
+            Pane newLoadedPane =  FXMLLoader.load(getClass().getResource("/at/fhtw/tourPlanner/routeWindow.fxml"));
+            contentWindowPane.getChildren().add(newLoadedPane);
+        } catch(Exception e){
+            System.out.println("Problem loading FXML into Pane");
+            e.printStackTrace();
+        }
     }
 
     public void loadMiscWindow(){
-        System.out.println("misc window load");
+        System.out.println("load misc window");
+
+        try{
+            contentWindowPane.getChildren().clear();
+
+            Pane newLoadedPane =  FXMLLoader.load(getClass().getResource("/at/fhtw/tourPlanner/routeWindow.fxml"));
+            contentWindowPane.getChildren().add(newLoadedPane);
+        } catch(Exception e){
+            System.out.println("Problem loading FXML into Pane");
+            e.printStackTrace();
+        }
     }
 }
